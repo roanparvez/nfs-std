8af3d0a223da55f88604627df497056613ad83a3
roanparvez <roanparvez@gmail.com>
2025-06-14 04:18:22 +0600

    chore: setup prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ export default function Home() {
   return (
     <>
       <p>
-        Professional Next.js Starter with Prettier, ESLint, Husky & lint-staged
-        — zero-config, production-ready.
+        Professional Next.js Starter with Prettier, ESLint, Husky & lint-staged — zero-config,
+        production-ready.
       </p>
     </>
   );
