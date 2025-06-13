8d7ab9297422830c9e5aac426c1210de70c03c5e
roanparvez <roanparvez@gmail.com>
2025-06-14 05:01:08 +0600

    chore: setup husky, lint-staged and update scripts for eslint, prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.config({
-    extends: ["next/core-web-vitals", "next/typescript"],
+    extends: ["next/core-web-vitals", "next/typescript", "prettier"],
     rules: {
       "@typescript-eslint/no-unused-vars": ["warn"],
 
