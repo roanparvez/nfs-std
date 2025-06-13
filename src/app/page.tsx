fc19bfcc46c347721ac971d7faa2874d6dc9a7cd
roanparvez <roanparvez@gmail.com>
2025-06-14 04:04:15 +0600

    init: create Next app

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <>
+      <p>
+        Professional Next.js Starter with Prettier, ESLint, Husky & lint-staged
+        — zero-config, production-ready.
+      </p>
+    </>
+  );
+}
